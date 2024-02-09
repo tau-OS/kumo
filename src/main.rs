1a0ac2030caf55e683f1f5fff3950bb5c04d71ce
Cappy Ishihara <cappy@cappuchino.xyz>
2024-02-09 22:44:50 +0700

    rewrite: Experiment with Blueprint and Fleet window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-mod wf_ipc;
-mod wf_socket;
 use color_eyre::Result;
-use serde_json::json;
 fn main() -> Result<()> {
     dotenvy::dotenv()?;
 
@@ -13,25 +10,6 @@ fn main() -> Result<()> {
             std::env::var("KUMO_LOG").unwrap_or_else(|_| "info".to_string()),
         ))
         .init();
-
-    let socket = wf_socket::get_wayfire_socket();
-    tracing::info!(?socket, "Wayfire Socket");
-
-    let mut socket = wf_socket::WayfireSocket::new();
-
-    loop {
-        tracing::info!("Sent watch command");
-        socket.send_json(json!(
-            {
-                "method": "window-rules/events/watch",
-                "data": {}
-            }
-        ))?;
-
-        let response = socket.read_message()?;
-
-        tracing::info!(?response, "Received response");
-    }
 
     Ok(())
 }
