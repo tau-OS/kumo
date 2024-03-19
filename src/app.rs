84d74333ee301e7fc4043aa0395028b0bd877e79
Cappy Ishihara <cappy@cappuchino.xyz>
2024-03-19 22:19:56 +0700

    Tick clock locally!

--- src/app.rs
+++ src/app.rs
@@ -3,7 +3,6 @@ use std::time::Duration;
 use crate::widgets::bar::Bar;
 use crate::widgets::fleet::Fleet;
 use gtk::prelude::*;
-use std::sync::{Mutex, RwLock};
 
 // todo: Maybe use D-Bus to talk through different components? Instead of using mpsc?
 
@@ -24,24 +23,20 @@ impl Application {
             fleet.set_application(Some(app));
             fleet.present();
 
-            // todo: glib::timeout_add for ticking clock
-            /*
-            glib::timeout_add(Duration::from_millis(500), move || {
-                // fleet.tick_clock();
+            glib::timeout_add_local(Duration::from_millis(500), move || {
+                fleet.tick_clock();
 
                 glib::clone!(@weak fleet => move || {
                     fleet.tick_clock();
-                });
-                glib::ControlFlow::Continue
-            }); */
+                })();
 
+                glib::ControlFlow::Continue
+            });
             let bar = Bar::new();
             // app.add_window(&bar);
             bar.set_application(Some(app));
             bar.set_child_visible(false);
             bar.present();
-
-            // Use fleet_box, fleet_label, fleet_window in the code
         });
 
         Application { app }
