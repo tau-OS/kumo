c320b56d588130e4ac2fa943e02a9713b1a0808d
Cappy Ishihara <cappy@cappuchino.xyz>
2024-07-03 23:57:52 +0700

    wip: appindicator fuckery

--- src/widgets/fleet/tray/imp.rs
+++ src/widgets/fleet/tray/imp.rs
@@ -1,14 +1,41 @@
-use glib::subclass::{object::{ObjectImpl, ObjectImplExt}, types::ObjectSubclass};
-use gtk::{prelude::{BoxExt, ButtonExt}, subclass::{box_::BoxImpl, widget::WidgetImpl}};
+use appindicator3::{prelude::AppIndicatorExt, Indicator};
+use glib::subclass::{
+    object::{ObjectImpl, ObjectImplExt},
+    types::ObjectSubclass,
+};
+use gtk::{
+    prelude::{BoxExt, ButtonExt},
+    subclass::{box_::BoxImpl, widget::WidgetImpl},
+};
 
 /// A representation of an AppIndicator icon
 /// Should be clickable and have a context menu
 pub struct SystemTrayItem {
+    pub indicator: Indicator,
     pub icon: gtk::Image,
     pub menu: gtk::PopoverMenu,
 
     /// The box to contain all the widgets inside here
     box_: gtk::Box,
+}
+
+impl From<Indicator> for SystemTrayItem {
+    fn from(indicator: Indicator) -> Self {
+        let icon = gtk::Image::new();
+        // todo: cleanup
+        let icon_name = indicator.icon_name();
+        let icon_name = icon_name.as_deref().unwrap_or("missing-icon");
+        icon.set_from_icon_name(Some(icon_name));
+        let menu = gtk::PopoverMenu::builder().build();
+        let box_ = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        box_.append(&icon);
+        Self {
+            icon,
+            indicator,
+            menu,
+            box_,
+        }
+    }
 }
 
 #[glib::object_subclass]
@@ -18,11 +45,7 @@ impl ObjectSubclass for SystemTrayItem {
     type ParentType = gtk::Box;
 
     fn new() -> Self {
-        Self {
-            icon: gtk::Image::new(),
-            menu: gtk::PopoverMenu::builder().build(),
-            box_: gtk::Box::new(gtk::Orientation::Vertical, 0),
-        }
+        todo!()
     }
 }
 
@@ -36,7 +59,6 @@ impl ObjectImpl for SystemTrayItem {
         self.box_.append(&button)
     }
 }
-
 
 pub struct SystemTray {
     // Box of SystemTrayIcons
